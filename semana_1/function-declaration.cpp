7d70fe9146397cb0fc686588d5e982e9a3ca9720
Tamicktom <rickpokemon38@gmail.com>
2025-07-25 20:28:59 -0300

    Add basic arithmetic functions for sum, subtraction, multiplication, and division

---
+++ semana_1/function-declaration.cpp
@@ -0,0 +1,25 @@
+
+int sum(int number1, int number2)
+{
+  return number1 + number2;
+}
+
+int sub(int number1, int number2)
+{
+  return number1 - number2;
+}
+
+int mul(int number1, int number2)
+{
+  return number1 * number2;
+}
+
+float div(float number1, float number2)
+{
+  return number1 / number2;
+}
+
+float idiv(int number1, int number2)
+{
+  return (float)number1 / (float)number2;
+}
